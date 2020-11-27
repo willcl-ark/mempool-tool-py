b4398dfea560892cf42b5978ad344f7b04bee9e6
willcl-ark <will8clark@gmail.com>
2020-11-27 15:51:09 +0000

    Improve reorg analysis
    Dynamically scale the result base on a hashpower input.
    
    Add a scaling factor of 75/hash_pow, this gives a 50% chance of re-org when fees
    of current block` are 1.5x fees of `next block` for hash_power of 50%
    
        75% hashpower: scale = 1
        50% hashpower: scale = 1.5
        25% hashpower: scale = 2

--- analyse.py
+++ analyse.py
@@ -2,13 +2,20 @@ import math
 from tabulate import tabulate
 
 
-def estimate_reorg(val):
-    # Estimate how likely someone is to perhaps try a re-org.
-    # This does not consider hash power which would also affect the likelihood.
+def estimate_reorg(val, hash_pow):
+    """
+    Estimate how likely someone is to perhaps try a re-org.
+    """
     # Error function: https://en.wikipedia.org/wiki/Error_function
-    # Add a scaling factor of 1.5, this gives a 50% chance of re-org when fees
-    # of current block` are 1.5x fees of `next block`
-    scale = 1.5
+
+    # Add a scaling factor of 75/hash_pow, this gives a 50% chance of re-org when fees
+    # of current block` are 1.5x fees of `next block` for hash_power of 50%
+
+    # 75% hashpower: scale = 1
+    # 50% hashpower: scale = 1.5
+    # 25% hashpower: scale = 2
+    scale = 75 / hash_pow
+
     return math.erf(val - scale)
 
 
@@ -21,12 +28,19 @@ def print_blocks(blocks: list):
         else:
             blocks[i].ratio = blocks[i].reward / blocks[i-1].reward
 
+    # Estimated reorg probability for 10, 25 and 50% hashpower miners
     for i in range(len(blocks)):
         if i == 0:
-            blocks[i].reorg = 0
+            blocks[i].reorg5 = 0
+            blocks[i].reorg10 = 0
+            blocks[i].reorg25 = 0
+            blocks[i].reorg50 = 0
         else:
             reward_diff = blocks[i-1].reward / blocks[i].reward
-            blocks[i].reorg = estimate_reorg(reward_diff)
+            blocks[i].reorg5 = estimate_reorg(reward_diff, 5)
+            blocks[i].reorg10 = estimate_reorg(reward_diff, 10)
+            blocks[i].reorg25 = estimate_reorg(reward_diff, 25)
+            blocks[i].reorg50 = estimate_reorg(reward_diff, 50)
 
     table = [
         ["tip offset"],             # 0
@@ -37,7 +51,10 @@ def print_blocks(blocks: list):
         ["reward / prev block"],    # 5
         ["weight"],                 # 6
         ["sigops", "N/A", "N/A"],   # 7
-        ["reorg chance"],           # 8
+        ["reorg chance 5% hash"],       # 8
+        ["reorg chance 10% hash"],       # 9
+        ["reorg chance 25% hash"],       # 10
+        ["reorg chance 50% hash"],       # 11
     ]
 
     table[0].extend([block.tip_offset for block in blocks])
@@ -48,7 +65,10 @@ def print_blocks(blocks: list):
     table[5].extend([f"{100*block.ratio:.3f}" for block in blocks])
     table[6].extend([f"{block.weight:,}" for block in blocks])
     table[7].extend([f"{block.sigopscost:,}" for block in blocks[2:]])
-    table[8].extend([f"{block.reorg:.5f}" for block in blocks])
+    table[8].extend([f"{block.reorg5:.5f}" for block in blocks])
+    table[9].extend([f"{block.reorg10:.5f}" for block in blocks])
+    table[10].extend([f"{block.reorg25:.5f}" for block in blocks])
+    table[11].extend([f"{block.reorg50:.5f}" for block in blocks])
 
     table_headers = ["block:"]
     table_headers.extend([block.height for block in blocks])
